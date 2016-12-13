c120380e4b35745de144aa9b2db16e2b169f4a7a
CaioRS <caiorss.rodrigues@gmail.com>
2016-12-13 14:14:16 -0300

    Fix IPFile module comments.

--- Pget.fs
+++ Pget.fs
@@ -18,9 +18,8 @@ module Framework =
     let net40 =  ".NETFramework,Version=v4.0"
     let net45 =  ".NETFramework,Version=v4.5"
 
-//  Wrapper for Nuget.IPackageFile class
-//
-//
+///  Wrapper for Nuget.IPackageFile class
+///
 module IPFile =
 
     type T = NuGet.IPackageFile
