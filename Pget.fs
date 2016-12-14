c3d61f7d5189945c76f8cbb481a6c9ad43cf4044
CaioRS <caiorss.rodrigues@gmail.com>
2016-12-14 12:20:39 -0300

    Add documentation to function zipPackage.

--- Pget.fs
+++ Pget.fs
@@ -149,7 +149,7 @@ module IPack =
         |> Seq.groupBy (fun (p: NuGet.IPackageFile) -> p.EffectivePath)
         |> Seq.map (fun (k, v) -> System.IO.Path.Combine(repoPath, fname, IPFile.path <| Seq.last v))
 
-
+    /// Read a .nupkg file.
     let zipPackage (nupkgFile: string) =  NuGet.ZipPackage nupkgFile
 
     /// Get a package's dependencies and its versions
