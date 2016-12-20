96c96c4cda9401c7f1e725028073ee3d211094b0
CaioRS <caiorss.rodrigues@gmail.com>
2016-12-20 19:35:27 -0300

    Update Assembly attributes.

--- src/AssemblyInfo.fs
+++ src/AssemblyInfo.fs
@@ -12,7 +12,7 @@ open System.Runtime.InteropServices;
 [<assembly: AssemblyConfiguration("")>]
 [<assembly: AssemblyCompany("")>]
 [<assembly: AssemblyProduct("pget.exe")>]
-[< assembly: AssemblyCopyright("Caio Rodrigues © AA. All rights reserved.")>]
+[< assembly: AssemblyCopyright("Public Domain Application.")>]
 //[ < assembly: AssemblyTrademark("") > ]
 [< assembly: AssemblyCulture("")>]
 
@@ -34,6 +34,6 @@ open System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the ‘*’ as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[<assembly: AssemblyVersion("1.0.0") >]
-[<assembly: AssemblyFileVersion("1.0.0")>]
+[<assembly: AssemblyVersion("1.2.0") >]
+[<assembly: AssemblyFileVersion("1.2.0")>]
 ()
