00a7a099657c25dba1dc4abe6ef1b840cadf9ebe
CaioRS <caiorss.rodrigues@gmail.com>
2017-01-04 23:24:29 -0300

    Update function showAsmHelp.

--- src/PgetCmd.fs
+++ src/PgetCmd.fs
@@ -211,8 +211,8 @@ module Main =
     asm --info [file]                                    Show all assembly attributes from an assembly [file].
     asm --refs [file]                                    Show all assembly references from an assembly [file].
     asm --resources  [file]                              Show resources from an assembly file.
-    asm --namespace|-ns [file]                           Display all assembly namespaces.
-    asm --namespace|-ns [file] --classes|-cls [ns]       Show all classes from namespace [ns] in assembly [file]
+    asm --namespace|-ns [file]                           Show all exported namespaces.
+    asm --namespace|-ns [file] [nspace]                  Show all types within an exported namespace from an assembly [file].
 
     asm --type [file]                                    Show all types exported by assembly [file]
     asm --type [file] [type]                             Show information about [type] exported by assembly [file].
@@ -221,9 +221,6 @@ module Main =
    
     asm --class  [file]                                  Show all classes exported by assembly [file].
     asm --classn [file]                                  Show all non-abstract classes exported by assembly [file]
-    asm --class [file] --public  [class]                 Show all public non-static methods of a [class] in a assembly [file]
-    asm --class [file] --static  [class]                 Show all static methods of a [class] in a assembly [file]
-    asm --class [file] --methods [class]                 Show all methods of a [class] in a assembly [file].        
         """
 
     let showHelp () =
