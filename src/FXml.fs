5b8d063acb40379de50bb898a6dd04d7f0fd46a9
CaioRS <caiorss.rodrigues@gmail.com>
2017-01-08 01:20:34 -0300

    Add module Pred - predicate comnbinators.

--- src/FXml.fs
+++ src/FXml.fs
@@ -3,6 +3,20 @@ module FXml
 open System
 open System.Xml 
 
+/// Predicate combinators
+module Pred =
+
+    let pAnd fa fb =
+        fun x -> fa x && fb x
+
+    let pOr fa fb =
+        fun x -> fa x || fb x
+
+    let pNot fa x = not (fa x)
+
+    module Operators =
+        let (<&&>) = pAnd
+        let (<||>) = pOr
 
 /// Functional wrapper around XmlNode object 
 module Node =
